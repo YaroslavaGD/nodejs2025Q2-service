8eaa94f8f9719cf5c7a962f00199988a894836f1
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2025-05-29 16:25:02 +0200

    init: add port in env

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = Number(process.env.PORT) || 4000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  await app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+  });
 }
 bootstrap();
