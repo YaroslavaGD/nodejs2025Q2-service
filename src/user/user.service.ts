32aa9b40e0326d516a824b8462a382ab35f2717f
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2025-05-30 15:57:30 +0200

    feat: add user resource

---
+++ src/user/user.service.ts
@@ -0,0 +1,73 @@
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { randomUUID } from 'node:crypto';
+
+@Injectable()
+export class UserService {
+  private users: User[] = [];
+
+  private omitPassword(user: User): Omit<User, 'password'> {
+    const userCopy = { ...user };
+    delete userCopy.password;
+    return userCopy;
+  }
+
+  create(dto: CreateUserDto) {
+    const time = Date.now();
+
+    const newUser: User = {
+      id: randomUUID(),
+      login: dto.login,
+      password: dto.password,
+      version: 1,
+      createdAt: time,
+      updatedAt: time,
+    };
+
+    this.users.push(newUser);
+
+    return this.omitPassword(newUser);
+  }
+
+  findAll(): Omit<User, 'password'>[] {
+    return this.users.map(this.omitPassword);
+  }
+
+  findOne(id: string): Omit<User, 'password'> {
+    const user = this.users.find((u) => u.id === id);
+
+    if (!user) throw new NotFoundException(`User with id ${id} not found`);
+
+    return this.omitPassword(user);
+  }
+
+  update(id: string, dto: UpdateUserDto): Omit<User, 'password'> {
+    const user = this.users.find((u) => u.id === id);
+
+    if (!user) throw new NotFoundException(`User with id ${id} not found`);
+    if (user.password !== dto.oldPassword) {
+      throw new ForbiddenException('Old password is incorrect');
+    }
+
+    user.password = dto.newPassword;
+    user.version++;
+    user.updatedAt = Date.now();
+
+    return this.omitPassword(user);
+  }
+
+  remove(id: string) {
+    const index = this.users.findIndex((u) => u.id === id);
+
+    if (index === -1)
+      throw new NotFoundException(`User with id ${id} not found`);
+
+    this.users.splice(index, 1);
+  }
+}
