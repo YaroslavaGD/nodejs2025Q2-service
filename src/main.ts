aa791b431a1f915778d3ea8c53b50384921ca17f
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2025-06-02 21:49:20 +0200

    feat: connect swagger in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { readFile } from 'node:fs/promises';
+import { join } from 'node:path';
+import { SwaggerModule } from '@nestjs/swagger';
+import { parse } from 'yaml';
 
 async function bootstrap() {
   const PORT = Number(process.env.PORT) || 4000;
@@ -8,8 +12,15 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
+  const docPath = join(__dirname, '..', 'doc', 'api.yaml');
+  const doc = await readFile(docPath, 'utf-8');
+  const swaggerDoc = parse(doc);
+  SwaggerModule.setup('doc', app, swaggerDoc);
+
   await app.listen(PORT, () => {
-    console.log(`Server started on http://localhost:${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}`);
+    console.log(`API documentation available at http://localhost:${PORT}/doc`);
   });
 }
+
 bootstrap();
